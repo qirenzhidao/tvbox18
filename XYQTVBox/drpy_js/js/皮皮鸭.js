5b79d7cad0a11ad33a47584332cc399a1d5bb685
qirenzhidao <192041411@qq.com>
2023-08-25 20:15:30 +0800

    backup xyq2023.8.24

--- XYQTVBox/drpy_js/js/皮皮鸭.js
+++ XYQTVBox/drpy_js/js/皮皮鸭.js
@@ -1,11 +1,13 @@
 // 地址发布页 https://www.ppydy.com
+// 一级筛选 数字验证
+
 muban.短视2.二级.tab_text = '.swiper-slide--i&&Text';
 var rule = {
 	title:'皮皮鸭',
 	模板:'短视2',
 	// host:'https://pipiya.cc',
 	host:'https://www.ppydy.com',
-	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,".link&&div:eq(1)&&Text");print(src);HOST=src',
+	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"a:eq(3)&&href");print(src);HOST=src',
 	homeUrl:'/label/rank.html',
 	class_name:'电影&国产剧&美剧&日韩剧&泰剧&其他剧&动漫&剧集',
 	class_url:'20&26&21&22&24&27&25&28',
@@ -24,7 +26,36 @@ var rule = {
 			"25":[{"key":"class","name":"类型","value":[{"n":"全部","v":""},{"n":"情感","v":"/class/情感"},{"n":"科幻","v":"/class/科幻"},{"n":"热血","v":"/class/热血"},{"n":"推理","v":"/class/推理"},{"n":"搞笑","v":"/class/搞笑"},{"n":"冒险","v":"/class/冒险"},{"n":"萝莉","v":"/class/萝莉"},{"n":"校园","v":"/class/校园"},{"n":"动作","v":"/class/动作"},{"n":"机战","v":"/class/机战"},{"n":"运动","v":"/class/运动"},{"n":"战争","v":"/class/战争"},{"n":"少年","v":"/class/少年"}]},{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"日本","v":"/area/日本"},{"n":"欧美","v":"/area/欧美"},{"n":"其他","v":"/area/其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"}]},{"key":"lang","name":"语言","value":[{"n":"全部","v":""},{"n":"日语","v":"/lang/日语"},{"n":"英语","v":"/lang/英语"},{"n":"其他","v":"/lang/其他"}]},{"key":"letter","name":"字母","value":[{"n":"全部","v":""},{"n":"A","v":"/letter/A"},{"n":"B","v":"/letter/B"},{"n":"C","v":"/letter/C"},{"n":"D","v":"/letter/D"},{"n":"E","v":"/letter/E"},{"n":"F","v":"/letter/F"},{"n":"G","v":"/letter/G"},{"n":"H","v":"/letter/H"},{"n":"I","v":"/letter/I"},{"n":"J","v":"/letter/J"},{"n":"K","v":"/letter/K"},{"n":"L","v":"/letter/L"},{"n":"M","v":"/letter/M"},{"n":"N","v":"/letter/N"},{"n":"O","v":"/letter/O"},{"n":"P","v":"/letter/P"},{"n":"Q","v":"/letter/Q"},{"n":"R","v":"/letter/R"},{"n":"S","v":"/letter/S"},{"n":"T","v":"/letter/T"},{"n":"U","v":"/letter/U"},{"n":"V","v":"/letter/V"},{"n":"W","v":"/letter/W"},{"n":"X","v":"/letter/X"},{"n":"Y","v":"/letter/Y"},{"n":"Z","v":"/letter/Z"},{"n":"0-9","v":"/letter/0-9"}]},{"key":"by","name":"排序","value":[{"n":"最新","v":"/by/time"},{"n":"最热","v":"/by/hits"},{"n":"评分","v":"/by/score"}]}]
 	},
 	timeout:5000,
+	play_parse:true,
+	lazy:`js:
+        var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
+        var url = html.url;
+        var from = html.from;
+        if (html.encrypt == '1') {
+            url = unescape(url)
+        } else if (html.encrypt == '2') {
+            url = unescape(base64Decode(url))
+        }
+        if (/m3u8|mp4/.test(url)) {
+            input = url
+        } else {
+            var MacPlayerConfig={};
+            eval(fetch(HOST + "/static/js/playerconfig.js").replace('var Mac','Mac'));
+            var jx = MacPlayerConfig.player_list[from].parse;
+            if (jx == '') {
+                jx = MacPlayerConfig.parse
+            };
+            if (jx.startsWith("/")) {
+                jx = jx = "https:" + jx;
+            }
+            input={jx:0,url:jx+url,parse:1,
+                header: JSON.stringify({
+                    'referer': input
+                })}
+        }
+    `,
 	detailUrl:'/voddetail/fyid.html',
+	// pagecount:{"20":1,"26":1,"21":1,"22":1,"24":1,"27":1,"25":1},
 	推荐:'.vod-rank-vod;a&&title;.lazy&&data-original;.vod-rank-state&&Text;a&&href',
 	一级:'.public-list-box;a&&title;.lazy&&data-original;.hide&&Text;a&&href',
 	// 一级:'json:list;vod_name;vod_pic;vod_remarks;vod_id',
